dee4e147f29254e456785ad07eac82f22a2ceb22
Headpenguin <headpenguin.penguinarts@gmail.com>
2022-05-23 18:49:39 -0400

    Moved random variable into an editor context struct so that it is easier to resuse code

--- src/Editor.rs
+++ src/Editor.rs
@@ -2,27 +2,15 @@
 extern crate BinaryFileIO;
 extern crate sdl2;
 
-use NinjaDungeon::{Map, Location, Tile, Direction, MAX_TILE_IDX, TileBuilder, TileBuilderSignals};
-use NinjaDungeon::Entities::Codes;
+use NinjaDungeon::{Map, Entity, EditorContext, self};
 
-use sdl2::hint;
 use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::mouse::MouseButton;
-use sdl2::keyboard::Scancode;
-use sdl2::rect::Rect;
-use sdl2::ttf::{self, Font};
-use sdl2::render::{TextureCreator, Texture};
-use sdl2::video::WindowContext;
 
-use BinaryFileIO::{load, dump};
 
-use std::io;
 use std::env;
 use std::fs;
-use std::str::FromStr;
 
-const DEFAULT_LOOKUP: &str = "MapName.txt";
+const DEFAULT_LOOKUP: &str = "Resources/MapName.txt";
 
 const WIDTH: u32 = 17*50;
 const HEIGHT: u32 = 13*50;
@@ -36,195 +24,31 @@ pub fn removeEntity(&mut self, position: (i32, i32)) -> Result<(), ()> {
 	self.entities.remove(self.entities.iter().position(|e| e.position == position).ok_or(())?);
 	Ok(())
 }*/
-struct Entity {
-	code: Codes,
-	position: (i32, i32),
-	direction: Direction,
-}
-fn main() {    
-	let SCREEN_RECT: Rect = Rect::new(0, 0, WIDTH, HEIGHT);
+fn main() {
+	let (textureCreator, ttfContext, mut editor) = EditorContext::new(WIDTH, HEIGHT, NAME, COLOR);
+
 	let file = match env::args().skip(1).next() {
 		Some(name) => name,
 		None => fs::read_to_string(DEFAULT_LOOKUP).unwrap_or_else(|_| {
-			println!("Warning: Could not find backup map file!");
-			String::from("")
+			eprintln!("Warning: Could not find any map filenames!");
+			String::from("map.out")
 		}),
 	};
 
-	let sdlContext = sdl2::init().unwrap();
-	let videoSubsystem = sdlContext.video().unwrap();
-	
-	if !hint::set("SDL_RENDER_SCALE_QUALITY", "1") {
-	
-		eprintln!("Warning: Linear texture filtering may not be enabled.");
-	
-	}
-
-	let window = videoSubsystem.window(NAME, WIDTH, HEIGHT)
-		.position_centered()
-		.build()
-		.unwrap();
-	
-	let mut canvas = window.into_canvas()
-		.present_vsync()
-		.build()
-		.unwrap();
-
-	let textureCreator = canvas.texture_creator();
-
-	let ttfContext = ttf::init().unwrap();
+	let mut map = NinjaDungeon::loadMap(&file, "Resources/Images/Map1.anim", &textureCreator).unwrap_or_else(|_| {
+		eprintln!("Warning: Could not read map file \"{}\"", &file);
+		let mut map = Map::new(0, "Resources/Images/Map1.anim", &textureCreator).unwrap();
+		map.addScreen(17, 12, (0, 0));
+		map
+	});
 
 	let font = ttfContext.load_font("Resources/Font/Symbola_hint.ttf", 16).unwrap();
 
 	let mut fontTexture = None;
 
-	let textInput = videoSubsystem.text_input();
-
-	let map: io::Result<(Map,)> = unsafe{load!(&file, map)};
-
-	let mut map: Map = match map {
-		Ok((mut map,)) => unsafe {map.createRenderer("Resources/Images/Map1.anim", &textureCreator); map},
-		Err(..) => {
-			println!("Warning: Could not read map file \"{}\"", &file);
-			let mut map = Map::new(0, "Resources/Images/Map1.anim", &textureCreator).unwrap();
-			map.addScreen(17, 12, (0, 0));
-			map
-		},
-	};
-
 	let mut entities = Vec::<Entity>::new();
-
-	let mut currentTileId = 0u16;
-
-	let mut currentTile = Tile::new(currentTileId, 0).unwrap();
-
-	let tileRect = Rect::new(0, HEIGHT as i32 - 50, 50, 50);
-
-	let mut events = sdlContext.event_pump().unwrap();
-
-	let mut builder = TileBuilder::new(currentTileId);
-
-	let mut state = State::Idle;
-
-	let mut lock = false;
-
-	let mut message = String::from("");
-
-	let mut messageLen = 0;
-
-	canvas.set_draw_color(COLOR);
-
-	let mut quit = false;
 	
-	while !quit {
-
-		canvas.clear();
-	
-		for event in events.poll_iter() {
-			if !lock {match event {
-				Event::Quit {..} => quit = true,
-
-				Event::MouseButtonDown {mouse_btn: MouseButton::Left, x, y, ..} 
-				if (y as i64) < (HEIGHT - 50) as i64 => {
-					builder = TileBuilder::new(currentTileId);
-					buil
-				},
-
-				Event::KeyDown{scancode: Some(Scancode::Left), ..} => {
-					if currentTileId > 0 {
-						currentTileId -= 1;
-						currentTile = Tile::new(currentTileId, 0).unwrap();
-					}
-				},
-				Event::KeyDown{scancode: Some(Scancode::Right), ..} => {
-					if currentTileId < MAX_TILE_IDX {
-						currentTileId += 1;
-						currentTile = Tile::new(currentTileId, 0).unwrap();
-					}
-				},
-				Event::KeyDown{scancode: Some(Scancode::S), ..} => {
-					dump!(&file, map).unwrap();
-				},	
-				Event::KeyDown{scancode: Some(Scancode::N), ..} => {
-					map.addScreen(17, 12, (0, 0));
-				},
-				Event::KeyDown{scancode: Some(Scancode::A), ..} => {
-					map.decrementCurrentScreen();
-				},
-				Event::KeyDown{scancode: Some(Scancode::D), ..} => {
-					map.incrementCurrentScreen();
-				},
-				_ => (),
-			}}
-			else {match (event, &state) {
-				(Event::Quit {..}, _) => quit = true,
-				(_, State::Idle) => lock = false,
-				(Event::KeyDown {scancode: Some(Scancode::Escape), ..}, _) => {
-					lock = false;
-					state = State::Idle;
-					fontTexture = None;
-				}
-				(Event::KeyDown {scancode: Some(Scancode::Return), ..}, State::GetUserUsize) => {
-					println!("{}", &message[messageLen..]);
-					if let Ok(id) = usize::from_str(&message[messageLen..].trim()) {
-						lock = false;
-						builder.addUsize(id);
-						state = State::AttemptBuild;
-						fontTexture = None;
-					}
-					else {
-						message.truncate(messageLen);
-						fontTexture = Some(font.render(&message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
-					}
-				},
-				(Event::TextInput {text, ..}, _) => {
-					message.push_str(&text);
-					fontTexture = Some(font.render(&message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
-				},
-				(Event::KeyDown {scancode: Some(Scancode::Backspace), ..}, _) if message.len() > messageLen => {
-					message.pop();
-					fontTexture = Some(font.render(&message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
-				},
-				_ => (),
-			}}
-		}
-
-		map.draw(&mut canvas);
-
-		if let Some(ref texture) = fontTexture {
-			let q = texture.query();
-			canvas.copy(texture, None, Some(Rect::from_center(SCREEN_RECT.center(), q.width, q.height)));
-		}
-
-		map.renderTile(tileRect, &currentTile, &mut canvas);
-		
-		canvas.present();
-
+	while !editor.mainLoop(&file, &mut map, &font, &mut fontTexture, &textureCreator) {
 	}
-}
-
-fn build<'a>(builder: &mut TileBuilder, map: &mut Map, state: &mut State, lock: &mut bool, 
-	message: &mut String, messageLen: &mut usize, fontTexture: &'a mut Option<Texture<'a>>, 
-	font: &Font, textureCreator: &'a TextureCreator<WindowContext>, x: &i32, y: &i32) {
-	match builder.build() {
-		TileBuilderSignals::GetUserUsize(tmpMessage) => {
-			*state = State::GetUserUsize;
-			*lock = true;
-			*message = String::from(tmpMessage);
-			*messageLen = tmpMessage.len() - 1;
-			*fontTexture = Some(font.render(&message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
-		},
-		TileBuilderSignals::Complete(tile) => {
-			*state = State::Idle;
-			map.changeTile(((x / 50) as u16, (y / 50) as u16), tile);
-		},
-		TileBuilderSignals::InvalidId => (),
-	}	
-}
-
-enum State {
-	GetUserUsize,
-	AttemptBuild,
-	Idle,
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,22 @@ extern crate sdl2;
 extern crate BinaryFileIO;
 
 use sdl2::{Sdl, VideoSubsystem, EventPump};
-use sdl2::render::{Canvas, TextureCreator};
+use sdl2::render::{Canvas, TextureCreator, Texture};
 use sdl2::video::{WindowContext, Window};
 use sdl2::event::Event;
 use sdl2::hint;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
+use sdl2::ttf::{Sdl2TtfContext, Font, self};
+use sdl2::keyboard::{TextInputUtil, Scancode};
+use sdl2::mouse::MouseButton;
 
-use BinaryFileIO::load;
+use BinaryFileIO::{load, dump};
 
 use std::io;
+use std::fs;
+use std::env;
+use std::str::FromStr;
 
 mod PlayerMod;
 mod SpriteLoader;
@@ -28,6 +34,8 @@ pub use PlayerMod::Player;
 pub use MapMod::{Map, Location, Tile, MAX_TILE_IDX, CollisionType, CollisionBounds, TileBuilder, TileBuilderSignals};
 
 use PlayerMod::SignalsBuilder;
+
+use Entities::Codes;
 
 pub struct GameContext {
 	sdlContext: Sdl,
@@ -109,6 +117,184 @@ impl GameContext {
 	}
 }
 
+pub struct EditorContext {
+	sdlContext: Sdl,
+	videoSubsystem: VideoSubsystem,
+	canvas: Canvas<Window>,
+	events: EventPump,
+	textInput: TextInputUtil,
+	quit: bool,
+	currentTileId: u16,
+	currentTilePosition: (u16, u16),
+	currentTile: Tile,
+	previewRect: Rect,
+	tileBuilder: TileBuilder,
+	screenRect: Rect,
+	state: State,
+	lock: bool,
+	message: String,
+	messageLen: usize,
+}
+
+impl EditorContext {
+	pub fn new(width: u32, height: u32, name: &'static str, color: Color,) -> (TextureCreator<WindowContext>, Sdl2TtfContext, EditorContext) {
+
+		let sdlContext = sdl2::init().unwrap();
+		let videoSubsystem = sdlContext.video().unwrap();
+		
+		if !hint::set("SDL_RENDER_SCALE_QUALITY", "1") {
+		
+			eprintln!("Warning: Linear texture filtering may not be enabled.");
+		
+		}
+
+		let window = videoSubsystem.window(name, width, height)
+			.position_centered()
+			.build()
+			.unwrap();
+		
+		let mut canvas = window.into_canvas()
+			.present_vsync()
+			.build()
+			.unwrap();
+
+		let textureCreator = canvas.texture_creator();
+		
+		let ttfContext = ttf::init().unwrap();
+
+		canvas.set_draw_color(color);
+
+		(textureCreator, ttfContext, EditorContext {
+			canvas,
+			events: sdlContext.event_pump().unwrap(),
+			textInput: videoSubsystem.text_input(),
+			sdlContext,
+			videoSubsystem,
+			quit: false,
+			currentTileId: 0,
+			currentTilePosition: (0, 0),
+			currentTile: Tile::new(0, 0).unwrap(),
+			screenRect: Rect::new(0, 0, width, height),
+			previewRect: Rect::new(0, height as i32 - 50, 50, 50),
+			tileBuilder: TileBuilder::new(0),
+			state: State::Idle,
+			lock: false,
+			message: String::new(),
+			messageLen: 0,
+		})
+	}
+
+	pub fn mainLoop<'a>(&mut self, filename: &str, map: &mut Map, font: &Font, fontTexture: &mut Option<Texture<'a>>, textureCreator: &'a TextureCreator<WindowContext>) -> bool {
+		self.canvas.clear();
+	
+		for event in self.events.poll_iter() {
+			if !self.lock {match event {
+				Event::Quit {..} => self.quit = true,
+
+				Event::MouseButtonDown {mouse_btn: MouseButton::Left, x, y, ..} 
+				if (y as i64) < (self.screenRect.height() - 50) as i64 => {
+					self.tileBuilder = TileBuilder::new(self.currentTileId);
+					self.currentTilePosition = ((x / 50) as u16, (y / 50) as u16);
+					self.state = State::AttemptBuild;
+					break;
+				},
+
+				Event::KeyDown{scancode: Some(Scancode::Left), ..} => {
+					if self.currentTileId > 0 {
+						self.currentTileId -= 1;
+						self.currentTile = Tile::new(self.currentTileId, 0).unwrap();
+					}
+				},
+				Event::KeyDown{scancode: Some(Scancode::Right), ..} => {
+					if self.currentTileId < MAX_TILE_IDX {
+						self.currentTileId += 1;
+						self.currentTile = Tile::new(self.currentTileId, 0).unwrap();
+					}
+				},
+				Event::KeyDown{scancode: Some(Scancode::S), ..} => {
+					dump!(filename, *map).unwrap();
+				},	
+				Event::KeyDown{scancode: Some(Scancode::N), ..} => {
+					map.addScreen(17, 12, (0, 0));
+				},
+				Event::KeyDown{scancode: Some(Scancode::A), ..} => {
+					map.decrementCurrentScreen();
+				},
+				Event::KeyDown{scancode: Some(Scancode::D), ..} => {
+					map.incrementCurrentScreen();
+				},
+				_ => (),
+			}}
+			else {match (event, &self.state) {
+				(Event::Quit {..}, _) => self.quit = true,
+				(_, State::Idle) => self.lock = false,
+				(Event::KeyDown {scancode: Some(Scancode::Escape), ..}, _) => {
+					self.lock = false;
+					self.state = State::Idle;
+					*fontTexture = None;
+				}
+				(Event::KeyDown {scancode: Some(Scancode::Return), ..}, State::GetUserUsize) => {
+					println!("{}", &self.message[self.messageLen..]);
+					if let Ok(id) = usize::from_str(&self.message[self.messageLen..].trim()) {
+						self.lock = false;
+						self.tileBuilder.addUsize(id);
+						self.state = State::AttemptBuild;
+						*fontTexture = None;
+					}
+					else {
+						self.message.truncate(self.messageLen);
+						*fontTexture = Some(font.render(&self.message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
+					}
+				},
+				(Event::TextInput {text, ..}, _) => {
+					self.message.push_str(&text);
+					*fontTexture = Some(font.render(&self.message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
+				},
+				(Event::KeyDown {scancode: Some(Scancode::Backspace), ..}, _) if self.message.len() > self.messageLen => {
+					self.message.pop();
+					*fontTexture = Some(font.render(&self.message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
+				},
+				_ => (),
+			}}
+		}
+
+		match self.state {
+			State::AttemptBuild => self.build(map, font, fontTexture, textureCreator),
+			_ => (),
+		}
+
+		map.draw(&mut self.canvas);
+
+		if let Some(ref texture) = fontTexture {
+			let q = texture.query();
+			self.canvas.copy(texture, None, Some(Rect::from_center(self.screenRect.center(), q.width, q.height)));
+		}
+
+		map.renderTile(self.previewRect, &self.currentTile, &mut self.canvas);
+		
+		self.canvas.present();
+
+		self.quit
+	}
+	fn build<'a>(&mut self, map: &mut Map, font: &Font, fontTexture: &mut Option<Texture<'a>>, textureCreator: &'a TextureCreator<WindowContext>) {
+		match self.tileBuilder.build() {
+			TileBuilderSignals::GetUserUsize(tmpMessage) => {
+				self.state = State::GetUserUsize;
+				self.lock = true;
+				self.message = String::from(tmpMessage);
+				self.messageLen = tmpMessage.len() - 1;
+				*fontTexture = Some(font.render(&self.message).shaded(Color::BLACK, Color::WHITE).unwrap().as_texture(&textureCreator).unwrap());
+			},
+			TileBuilderSignals::Complete(tile) => {
+				self.state = State::Idle;
+				map.changeTile(self.currentTilePosition, tile);
+			},
+			TileBuilderSignals::InvalidId => (),
+		}	
+	}
+
+}
+
 pub fn loadMap<'a>(filename: &str, tileSprites: &str, creator: &'a TextureCreator<WindowContext>) -> io::Result<Map<'a>> {
 	let map: io::Result<(Map,)> = unsafe{load!(filename, map)};
 
@@ -128,5 +314,17 @@ pub enum Direction {
 
 pub trait PlayerCollision {
     fn collidePlayer(&self, player: &mut Player);
+}
+
+pub struct Entity {
+	code: Codes,
+	position: (i32, i32),
+	direction: Direction,
+}
+
+enum State {
+	GetUserUsize,
+	AttemptBuild,
+	Idle,
 }
 
