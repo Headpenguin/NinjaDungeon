966884508c273c28fa15abd9bf9ca4998a8e964a
Andy Collins <headpenguin.penguinarts@gmail.com>
2023-06-09 07:51:50 -0400

    Begin adding entity generator

--- src/Entities/GeneratorMod.rs
+++ src/Entities/GeneratorMod.rs
@@ -21,6 +21,12 @@ pub struct InnerGenerator {
 	cnt: u8,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct InnerEntityGenerator {
+    gen: InnerGenerator,
+    entities: Vec<(ID, bool)>,
+}
+
 impl InnerGenerator {
 	pub fn fromGenerator(Generator { renderRect, tiles, cnt, .. }: &Generator) -> InnerGenerator {
 		InnerGenerator {
@@ -39,31 +45,37 @@ pub struct Generator<'a> {
 	cnt: u8,
 }
 
+#[derive(Debug)]
+pub struct EntityGenerator<'a> {
+    gen: Generator<'a>,
+    entities: Vec<(ID, bool)>,
+}
+
 impl<'a> Generator<'a> {
+	pub fn newInt(creator: &'a TextureCreator<WindowContext>, pos: (i32, i32), tiles: Vec<(Tile, (u16, u16))>, cnt: u8) -> io::Result<Self> {
+        Ok(Generator {
+            renderRect: Rect::new(pos.0, pos.1, 50, 50),
+            tiles,
+            editor: true,
+            sprite: Sprites::new(creator, NAME)?,
+            cnt,
+        })
+    }
 	pub fn new(creator: &'a TextureCreator<WindowContext>, pos: (i32, i32), tiles: Vec<(Tile, (u16, u16))>, cnt: u8) -> io::Result<BoxCode<'a>> {
 		Ok(BoxCode::Generator(
 			Entity::new(
-				Generator {
-					renderRect: Rect::new(pos.0, pos.1, 50, 50),
-					tiles,
-					editor: true,
-					sprite: Sprites::new(creator, NAME)?,
-					cnt,
-				},
+                Self::newInt(creator, pos, tiles, cnt)?,
 				()
 			)
 		))
 	}
+	pub fn fromInnerInt(InnerGenerator { renderRect, tiles, cnt }: InnerGenerator, creator: &'a TextureCreator<WindowContext>) -> io::Result<Self> {
+        Self::newInt(creator, renderRect.top_left(), tiles, cnt)
+    }
 	pub fn fromInner(InnerGenerator { renderRect, tiles, cnt }: InnerGenerator, creator: &'a TextureCreator<WindowContext>) -> io::Result<BoxCode<'a>> {
 		Ok(BoxCode::Generator(
 			Entity::new(
-				Generator {
-					renderRect: Rect::from(renderRect),
-					tiles,
-					editor: true,
-					sprite: Sprites::new(creator, NAME)?,
-					cnt,
-				},
+                Self::newInt(creator, renderRect.top_left(), tiles, cnt)?,
 				()
 			)
 		))
@@ -71,22 +83,74 @@ impl<'a> Generator<'a> {
 	pub fn collidesStatic(&self, hitbox: Rect) -> bool {
 		self.renderRect.has_intersection(hitbox)
 	}
-}
-
-impl<'a> Collision for Generator<'a> {
-	fn collideWith(&self, _other: ID, _po: &PO, key: Key) -> (Option<Envelope<CollisionMsg>>, Key) {(None, key)}
-}
-
-impl<'a> RegisterID for Generator<'a> {}
-
-impl<'a> Counter for Generator<'a> {
-	fn inc(&mut self, msg: Envelope<CounterMsg>, po: &PO) {
-		self.cnt = (self.cnt as i32 + msg.getMsg().0) as u8;
+    fn activate(&mut self, po: &PO) {
 		if self.cnt == 0 {
 			for (tile, location) in self.tiles.drain(..) {
 				po.spawnTile(tile, location);
 			}
 		}
+    }
+}
+
+impl<'a> EntityGenerator<'a> {
+	pub fn new(creator: &'a TextureCreator<WindowContext>, pos: (i32, i32), tiles: Vec<(Tile, (u16, u16))>, entities: Vec<(ID, bool)>, cnt: u8) -> io::Result<BoxCode<'a>> {
+		Ok(BoxCode::Generator(
+			Entity::new(
+                gen: Generator::newInt(creator, pos, tiles, cnt)?,
+                entities,
+				},
+				()
+			)
+		))
+	}
+	pub fn fromInner(InnerEntityGenerator {gen, entities}, creator: &'a TextureCreator<WindowContext>) -> io::Result<BoxCode<'a>> {
+		Ok(BoxCode::Generator(
+			Entity::new(
+				EntityGenerator {
+                    gen: Generator::fromInnerInt(gen)?,
+                    entities,
+				},
+				()
+			)
+		))
+	}
+	pub fn collidesStatic(&self, hitbox: Rect) -> bool {
+		self.gen.renderRect.has_intersection(hitbox)
+	}
+    fn activate(&mut self, po: &PO) {
+        if self.cnt == 0 {
+            for (entity, global) in self.entities.drain(..) {
+                if global {
+                    po.addEntityGlobal(entity);
+                }
+                else {
+                    po.addEntityActiveScreen(entity);
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Collision for Generator<'a> {
+	fn collideWith(&self, _other: ID, _po: &PO, key: Key) -> (Option<Envelope<CollisionMsg>>, Key) {(None, key)}
+}
+impl<'a> Collision for EntityGenerator<'a> {
+	fn collideWith(&self, _other: ID, _po: &PO, key: Key) -> (Option<Envelope<CollisionMsg>>, Key) {(None, key)}
+}
+
+impl<'a> RegisterID for Generator<'a> {}
+impl<'a> RegisterID for EntityGenerator<'a> {}
+
+impl<'a> Counter for Generator<'a> {
+	fn inc(&mut self, msg: Envelope<CounterMsg>, po: &PO) {
+		self.cnt = (self.cnt as i32 + msg.getMsg().0) as u8;
+        self.activate(po);
+	}
+}
+impl<'a> Counter for EntityGenerator<'a> {
+	fn inc(&mut self, msg: Envelope<CounterMsg>, po: &PO) {
+        self.gen.inc(msg, po);
+        self.activate(po);
 	}
 }
 
@@ -100,6 +164,33 @@ impl<'a> EntityTraitsWrappable<'a> for Generator<'a> {
 	}
 	fn mapCode<'b>(code: RefCode<'a, 'b>) -> Option<&'b Self> {
 		if let RefCode::Generator(gen) = code {
+			Some(gen)
+		}
+		else {None}
+	}
+	fn getData(&self, data: &mut Self::Data, po: &PO, key: Key) -> Key {key}
+	fn update(&mut self, data: &Self::Data, po: &mut PO) {
+		self.editor = false;
+	}
+	fn needsExecution(&self) -> bool {self.editor}
+	fn tick(&mut self) {}
+	fn draw(&self, canvas: &mut Canvas<Window>) {
+		if self.editor {
+			self.sprite.getSprite(0).draw(canvas, self.renderRect, false, false);
+		}
+	}
+	fn setID(&mut self, id: TypedID<'a, Self>) {}
+}
+impl<'a> EntityTraitsWrappable<'a> for EntityGenerator<'a> {
+	type Data = ();
+	fn mapCodeMut<'b>(code: RefCodeMut<'a, 'b>) -> Option<&'b mut Self> {
+		if let RefCodeMut::EntityGenerator(gen) = code {
+			Some(gen)
+		}
+		else {None}
+	}
+	fn mapCode<'b>(code: RefCode<'a, 'b>) -> Option<&'b Self> {
+		if let RefCode::EntityGenerator(gen) = code {
 			Some(gen)
 		}
 		else {None}
